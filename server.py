ba84151eb087745a3b5d1217029742afa3baeaa2
Fodé Guirassy <fode.guirassy@ekino.com>
2019-01-14 21:42:21 +0100

    local tests

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 coding = 'utf-8'
-
 from flask import Flask, request
 from PIL import Image
 import numpy as np
@@ -7,11 +6,10 @@ from sklearn.model_selection import train_test_split
 from sklearn import neighbors
 from collections import Counter
 import coremltools
-from sklearn import tree,svm
+from sklearn import tree, svm
 from sklearn.ensemble import RandomForestClassifier
 
 app = Flask(__name__)
-
 
 im = Image.open("non_contrarie.bmp")
 
@@ -32,11 +30,11 @@ for i in range(row_max):
         current_box = (x_start, y_start, x_start + x_delta, y_start + y_delta)
         current_region = im.crop(current_box)
         current_region = current_region.convert('L')
-        #current_region = current_region.convert()
+        # current_region = current_region.convert()
 
         pix = np.array(current_region)
         pix = pix.reshape((current_region.size[0] * current_region.size[1],))
-        #pix = pix.reshape((pix.shape[0], pix.shape[1] * pix.shape[2]))
+        # pix = pix.reshape((pix.shape[0], pix.shape[1] * pix.shape[2]))
 
         models_array.append(pix)
         labels_array.append(raw_labels_array[i])
@@ -48,14 +46,14 @@ for i in range(row_max):
     y_start = y_start + 120
 
 models_array = np.array(models_array)
-#models_array = models_array.reshape((models_array.shape[0], models_array.shape[1] * models_array.shape[2]))
+# models_array = models_array.reshape((models_array.shape[0], models_array.shape[1] * models_array.shape[2]))
 print("models Array shape {}".format(models_array.shape))
 print("Labels Array shape {}".format(np.array(labels_array).shape))
 
 zipped_array = np.array(list(zip(models_array, labels_array)))
-#print("Zipped Array {}".format(zipped_array))
+# print("Zipped Array {}".format(zipped_array))
 
-#clf = tree.DecisionTreeClassifier().fit(models_array, labels_array)
+# clf = tree.DecisionTreeClassifier().fit(models_array, labels_array)
 clf = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0, min_samples_leaf=1)
 clf.fit(models_array, labels_array)
 
@@ -64,12 +62,13 @@ clf.fit(models_array, labels_array)
 def hello():
     return "Hello World"
 
+
 @app.route("/add", methods=["POST"])
 def add_example():
     json = request.get_json(force=True)
 
-    #print(json["data"])
-    #print(json["label"])
+    # print(json["data"])
+    # print(json["label"])
 
     new_model = np.array(json["data"])
     new_label = json["label"]
@@ -93,3 +92,5 @@ def predict():
     predString = "{}".format(predict)
     print(predString)
     return predString[3:-2]
+
+#app.run(host='0.0.0.0', port=5000)
