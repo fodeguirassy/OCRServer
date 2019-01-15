cfbe679e973906a2a442e965ee618499e3b54b6c
Fodé Guirassy <fode.guirassy@ekino.com>
2019-01-15 15:09:34 +0100

    u prefix

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from sklearn.ensemble import RandomForestClassifier
 
 app = Flask(__name__)
 
-im = Image.open("non_contrarie.bmp")
+im = Image.open(u"non_contrarie.bmp")
 
 x_start = 70
 y_start = 50
@@ -18,14 +18,14 @@ col_max = 11
 
 labels_array = []
 models_array = []
-raw_labels_array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t"]
+raw_labels_array = [u"a", u"b", u"c", u"d", u"e", u"f", u"g", u"h", u"i", u"j", u"k", u"l", u"m", u"n", u"o", u"p", u"q", u"r", u"s", u"t"]
 raw_models_array = []
 
 for i in range(row_max):
     for j in range(col_max):
         current_box = (x_start, y_start, x_start + x_delta, y_start + y_delta)
         current_region = im.crop(current_box)
-        current_region = current_region.convert("L")
+        current_region = current_region.convert(u"L")
 
         pix = np.array(current_region)
         pix = pix.reshape((current_region.size[0] * current_region.size[1],))
@@ -51,7 +51,6 @@ def add_example():
         return json['label']
     else:
         return 'It is a GET'
-
 
 
 if __name__ == '__main__':
