9ceffe0de421107c92976c559a6710878cadc573
Fodé Guirassy <fode.guirassy@ekino.com>
2019-01-15 12:00:04 +0100

    test 5

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
 
@@ -9,4 +9,5 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0/api', port=port)
