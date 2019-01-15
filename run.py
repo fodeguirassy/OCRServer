33b4c06d51c6a9d17cb708a55443fc32d24a4859
Fodé Guirassy <fode.guirassy@ekino.com>
2019-01-15 13:18:28 +0100

    change encoding declaration again

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
-# This Python file uses the following encoding: utf-8
+# -*- coding: utf-8 -*-
 from flask import Flask, request
 from PIL import Image
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def add_example():
